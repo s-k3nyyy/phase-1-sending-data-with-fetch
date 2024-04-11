a45421a59c40c9348e08c083a61bcd996810fda0
s-k3nyyy <158499263+s-k3nyyy@users.noreply.github.com>
2024-04-11 12:02:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+ function submitData(username, email) {
+      return fetch('http://localhost:3000/cats', {
+      })
+      .then(function(response) {
+        if (!response.ok) {
+     ('Network response was not ok');
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        document.body.innerHTML = 'An error occurred: ' + error.message;
+        console.error(error);
+      });
+    }
